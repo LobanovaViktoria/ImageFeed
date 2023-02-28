1960a0b0b4a1533c281a4eb993c97cf8320924ba
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-02-28 17:05:17 +0300

    добавлены AuthViewController, WebViewViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Viktoria Lobanova on 28.02.2023.
+//
+
+import UIKit
+
+private class AuthViewController: UIViewController {
+    
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Viktoria Lobanova on 28.02.2023.
+//
+
+import UIKit
+import WebKit
+
+private class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+}
+
+
