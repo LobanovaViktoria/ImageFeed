d335cd1f82cdca4fd4eb1aab8bf7c8564773b4b1
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-03-02 10:33:51 +0300

    сформирован URL с помощью URLComponents WKNavigationDelegate

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -8,9 +8,74 @@
 import UIKit
 import WebKit
 
-private class WebViewViewController: UIViewController {
+private struct APIConstants {
+    static let authorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let code = "code"
+    static let authorizathionPath = "/oauth/authorize/native"
+}
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc:WebViewViewController)
+    }
+
+final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        print("Press button")
+    }
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        
+        loadWebView()
+    }
+    // webView.navigationDelegate = self
+}
+    private extension WebViewViewController {
+        func loadWebView() {
+            var urlComponents = URLComponents(string: APIConstants.authorizeURLString)
+            urlComponents?.queryItems = [
+            URLQueryItem(name: "client_id", value: accessKey),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: accessScope)
+            ]
+            if let url = urlComponents?.url {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
+        }
+        
+        func fetchCode(from url: URL?) -> String? {
+            if let url = url,
+            let components = URLComponents(string: url.absoluteString),
+               components.path == APIConstants.authorizathionPath,
+               let codeItem = components.queryItems?.first(where: { $0.name == APIConstants.code }) {
+                return codeItem.value
+            } else {
+                return nil
+            }
+        }
+    }
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+       // print("ITS LIT", navigationAction.request.url)
+       
+        if let code = fetchCode(from: navigationAction.request.url) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
 }
 
 
