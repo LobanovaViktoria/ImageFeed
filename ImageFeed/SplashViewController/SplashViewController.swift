d352f714612d1950365497100f8a4b22c2751f1a
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-03-07 11:32:32 +0300

    Подключение сторонней зависимости ProgressHUD

--- ImageFeed/SplashViewController/SplashViewController.swift
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 final class SplashViewController: UIViewController {
     
@@ -58,6 +59,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show(icon: .bolt)
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -70,7 +72,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
+                ProgressHUD.dismiss()
             case .failure:// TODO [Sprint 11]
+                ProgressHUD.dismiss()
                 break
             }
         }
