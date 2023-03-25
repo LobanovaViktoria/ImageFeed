552bbf5698b59bd18b7975807d36e6c0821fddd0
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-03-25 18:22:46 +0300

    Добавлена возможность просматривать увеличенную картинку

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -52,111 +52,105 @@ final class ImagesListViewController: UIViewController {
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == showSingleImageSegueIdentifier {
-            let viewController = segue.destination as? SingleImageViewController
-            let indexPath = sender as? IndexPath
-            guard let index = indexPath?.row else { return }
-            //            let image = UIImage(named: photosName[index])
-            //            viewController?.image = image
-        } else {
-            super.prepare(for: segue, sender: sender)
-        }
-    }
-    
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        if let urlString = imagesListService.photos[indexPath.row].thumbImageURL,
-           let imagesURL = URL(string: urlString) {
-            cell.cellImageView.kf.indicatorType = .activity
-            cell.cellImageView.kf.setImage(with: imagesURL,
-                                           placeholder: UIImage(named: "scribble")) { [weak self] _ in
-                guard let self = self else { return }
-                self.tableView.reloadRows(at: [indexPath], with: .automatic)
-            }
-            
-            cell.dateLabel.text = dateFormatter.string(from: imagesListService.photos[indexPath.row].createdAt ?? Date())
-            
-            let isLiked = imagesListService.photos[indexPath.row].isLiked == false
-            let likeImage = isLiked ? UIImage(named: "noActive") : UIImage(named: "yesActive")
-            cell.likeOrDislakeButton.setImage(likeImage, for: .normal)
-        }
-    }
-}
-
-extension ImagesListViewController: UITableViewDelegate {
-    
-    // Метод отвечает за действия, которые будут выполнены при тапе по ячейке таблицы
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
-    }
-    
-    // Метод, который вычисляет высоту ячейки
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let imagesHeight = imagesListService.photos[indexPath.row].size.height
-        let imagesWidth = imagesListService.photos[indexPath.row].size.width
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let heightForRowAt = imagesHeight * imageViewWidth / imagesWidth + imageInsets.top + imageInsets.bottom
-        return heightForRowAt
-    }
-}
-
-extension ImagesListViewController: UITableViewDataSource {
-    
-    // Метод, который определяет количество ячеек в секции таблицы
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imagesListService.photos.count
-    }
-    
-    // Если indexPath соответствует индексу последней строки в таблице, то вызывается fetchPhotosNextPage()
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == tableView.numberOfRows(inSection: 0) - 1 {
-            imagesListService.fetchPhotosNextPage()
-        }
-    }
-    
-    // Метод протокола, который возвращает ячейку
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-        
-        guard let imageListCell = cell as? ImagesListCell else {
-            return UITableViewCell()
-        }
-        imageListCell.delegate = self
-        
-        configCell(for: imageListCell, with: indexPath)
-        
-        return imageListCell
-    }
-}
-
-extension ImagesListViewController: ImagesListCellDelegate {
-    func imageListCellDidTapLike(_ cell: ImagesListCell) {
-        
-        guard let indexPath = tableView.indexPath(for: cell) else { return }
-        let photo = photos[indexPath.row]
-        // Покажем лоадер
-        UIBlockingProgressHUD.show()
-        imagesListService.changeLike(photoId: photo.id, isLike: photo.isLiked) { result in
-            switch result {
-            case.success:
-                // Синхронизируем массив картинок с сервисом
-                self.photos = self.imagesListService.photos
-                // Изменим индикацию лайка картинки
-                cell.setIsLiked(isLiked: self.photos[indexPath.row].isLiked)
-                // Уберем лоадер
-                UIBlockingProgressHUD.dismiss()
-            case.failure:
-                // Уберём лоадер
-                UIBlockingProgressHUD.dismiss()
-                // Покажем, что что-то пошло не так
-                // TODO: Показать ошибку с использованием UIAlertController
+        func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+            if let urlString = imagesListService.photos[indexPath.row].thumbImageURL,
+               let imagesURL = URL(string: urlString) {
+                cell.cellImageView.kf.indicatorType = .activity
+                cell.cellImageView.kf.setImage(with: imagesURL,
+                                               placeholder: UIImage(named: "scribble")) { [weak self] _ in
+                    guard let self = self else { return }
+                    self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                }
                 
+                cell.dateLabel.text = dateFormatter.string(from: imagesListService.photos[indexPath.row].createdAt ?? Date())
+                
+                let isLiked = imagesListService.photos[indexPath.row].isLiked == false
+                let likeImage = isLiked ? UIImage(named: "noActive") : UIImage(named: "yesActive")
+                cell.likeOrDislakeButton.setImage(likeImage, for: .normal)
+            }
+        }
+    }
+
+    extension ImagesListViewController: UITableViewDelegate {
+        
+        // Метод отвечает за действия, которые будут выполнены при тапе по ячейке таблицы
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            let viewController = SingleImageViewController()
+            
+            if let urlFullString = imagesListService.photos[indexPath.row].largeImageURL,
+               let imageFullURL = URL(string: urlFullString) {
+                viewController.imageURL = imageFullURL
+            }
+            viewController.modalPresentationStyle = .fullScreen
+            present(viewController, animated: true)
+        }
+        
+        // Метод, который вычисляет высоту ячейки
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            let imagesHeight = imagesListService.photos[indexPath.row].size.height
+            let imagesWidth = imagesListService.photos[indexPath.row].size.width
+            let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+            let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+            let heightForRowAt = imagesHeight * imageViewWidth / imagesWidth + imageInsets.top + imageInsets.bottom
+            return heightForRowAt
+        }
+    }
+    
+    extension ImagesListViewController: UITableViewDataSource {
+        
+        // Метод, который определяет количество ячеек в секции таблицы
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return imagesListService.photos.count
+        }
+        
+        // Если indexPath соответствует индексу последней строки в таблице, то вызывается fetchPhotosNextPage()
+        func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+            if indexPath.row == tableView.numberOfRows(inSection: 0) - 1 {
+                imagesListService.fetchPhotosNextPage()
             }
         }
         
+        // Метод протокола, который возвращает ячейку
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+            
+            guard let imageListCell = cell as? ImagesListCell else {
+                return UITableViewCell()
+            }
+            imageListCell.delegate = self
+            
+            configCell(for: imageListCell, with: indexPath)
+            
+            return imageListCell
+        }
     }
-}
-
+    
+    extension ImagesListViewController: ImagesListCellDelegate {
+        func imageListCellDidTapLike(_ cell: ImagesListCell) {
+            
+            guard let indexPath = tableView.indexPath(for: cell) else { return }
+            let photo = photos[indexPath.row]
+            // Покажем лоадер
+            UIBlockingProgressHUD.show()
+            imagesListService.changeLike(photoId: photo.id, isLike: photo.isLiked) { result in
+                switch result {
+                case.success:
+                    // Синхронизируем массив картинок с сервисом
+                    self.photos = self.imagesListService.photos
+                    // Изменим индикацию лайка картинки
+                    cell.setIsLiked(isLiked: self.photos[indexPath.row].isLiked)
+                    // Уберем лоадер
+                    UIBlockingProgressHUD.dismiss()
+                case.failure:
+                    // Уберём лоадер
+                    UIBlockingProgressHUD.dismiss()
+                    // Покажем, что что-то пошло не так
+                    // TODO: Показать ошибку с использованием UIAlertController
+                    
+                }
+            }
+            
+        }
+    }
 
--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -6,38 +6,90 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class SingleImageViewController: UIViewController {
-    var image: UIImage! {
-        didSet {
-            guard isViewLoaded else { return }
-            imageView.image = image
-            rescaleAndCenterImageInScrollView(image: image)
-        }
-    }
+    var imageURL: URL?
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
-    @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private weak var scrollView: UIScrollView!
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 7.25
+        return scrollView
+    }()
+    
+    @objc private lazy var backButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "nav_back_button")!,
+            target: self,
+            action: #selector(Self.didTapBackButtonSingleImage)
+        )
+        button.tintColor = .ypWhite
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "sharing"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = image
-        scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 7.25
-        rescaleAndCenterImageInScrollView(image: image)
+        addSubviews()
+        setupLayout()
+        loadImageView()
+    }
+
+    @objc
+    private func didTapBackButtonSingleImage() {
+        navigationController?.pushViewController(, animated: <#T##Bool#>)
     }
     
-    @IBAction private func didTapBackButton() {
-        dismiss(animated: true,  completion: nil)
+    @objc
+    private func didTapShareButton() {
+        
     }
     
-    @IBAction func didTapShareButton(_ sender: UIButton) {
-        guard let image = imageView.image else { return }
-        let share = UIActivityViewController(
-            activityItems: [image],
-            applicationActivities: nil
-        )
-        present(share, animated: true, completion: nil)
+    private func addSubviews() {
+        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        view.addSubview(backButton)
+        view.addSubview(shareButton)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            
+            
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            
+            shareButton.widthAnchor.constraint(equalToConstant: 50),
+            shareButton.heightAnchor.constraint(equalToConstant: 50),
+            shareButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            shareButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -36),
+            
+            backButton.widthAnchor.constraint(equalToConstant: 48),
+            backButton.heightAnchor.constraint(equalToConstant: 48),
+            
+            backButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        ])
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
@@ -55,6 +107,32 @@ final class SingleImageViewController: UIViewController {
         let x = (newContentSize.width - visibleRectSize.width) / 2
         let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    private func showError() {
+        let alertController = UIAlertController(
+            title: "Что-то пошло не так.",
+            message: "Попробовать еще раз?",
+            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Не надо", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Повторить", style: .default, handler: { action in
+            self.loadImageView()
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func loadImageView() {
+        UIBlockingProgressHUD.show()
+        self.imageView.kf.setImage(with: imageURL) { [weak self] result in
+            UIBlockingProgressHUD.dismiss()
+            guard let self = self else { return }
+            switch result {
+            case .success(let imageResult):
+                self.rescaleAndCenterImageInScrollView(image: imageResult.image)
+            case .failure:
+                self.showError()
+            }
+        }
     }
 }
 
